36f9683968b8540422af47ba62d308b40f15f064
Hlib13 <taronassvolt@gmail.com>
2024-10-29 23:41:10 +0200

    task_9

---
+++ src/main/java/com/tasks/LeetCode_CodeWars/LeetCodeCodeWarsApplication.java
@@ -0,0 +1,13 @@
+package com.tasks.LeetCode_CodeWars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetCodeCodeWarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetCodeCodeWarsApplication.class, args);
+	}
+
+}
